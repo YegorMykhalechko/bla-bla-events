539b6e1c2e1132285efba58782238056e1c6c778
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-02-28 19:59:17 +0100

    added firebase

---
+++ plugins/firebaseAuth.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.FIREBASE_API_KEY,
+    authDomain: config.FIREBASE_AUTH_DOMAIN,
+    projectId: config.FIREBASE_PROJECT_ID,
+    storageBucket: config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+    appId: config.FIREBASE_APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+});
