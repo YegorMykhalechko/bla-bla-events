123fedc7977d932a94d4920ef5d534dfe68349a8
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-02-28 21:11:55 +0100

    added composable

---
+++ composables/useFirebase.ts
@@ -0,0 +1,45 @@
+//https://firebase.google.com/docs/auth/web/start
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+export const createUser = async (email: any, password: any) => {
+  const auth = getAuth();
+  const credentials = createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  return credentials;
+};
+
+export const signInUser = async (email: any, password: any) => {
+  const auth = getAuth();
+  const credentials = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  return credentials;
+};
+
+export const initUser = async () => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(user);
+    } else {
+    }
+  });
+};
