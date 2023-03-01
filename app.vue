b188090b072145c998884a70a1e18d97fcf06614
YegorMykhalechko <yegor@unostella.com>
2023-03-01 16:05:10 +0100

    added credentails on ome

--- app.vue
+++ app.vue
@@ -1,30 +1,34 @@
 <template>
-  <div>
-    <button @click="signIn" class="button">Sign In</button>
-    <button @click="signOut" class="button">Sign Out</button>
-  </div>
+    <div>
+        <button @click="signIn" class="button">Sign In</button>
+        <button @click="signOut" class="button">Sign Out</button>
+        <pre>
+            {{credentials}}
+        </pre>
+    </div>
 </template>
 <script setup>
+const credentials = ref();
+
 const signIn = async () => {
-  const email = "test@test.com";
-  const password = "123456";
-  const credentials = await signInUser(email, password);
-  console.log("Credentials:", credentials);
+    const email = "test@test.com";
+    const password = "123456";
+    credentials.value = await signInUser(email, password);
 };
 
 const signOut = async () => {
-  const result = await signOutUser();
+    credentials.value = await signOutUser();
 };
 onMounted(async () => {
-  // const email = "test@test.com";
-  // const password = "123456";
-  // const credentials = await createUser(email, password);
-  // console.log(credentials);
-  // const email = "test@test.com";
-  // const password = "123456";
-  // const credentials = await signInUser(email, password);
-  // console.log(credentials)
-  // const result = await signOutUser()
-  // console.log(result)
+    // const email = "test@test.com";
+    // const password = "123456";
+    // const credentials = await createUser(email, password);
+    // console.log(credentials);
+    // const email = "test@test.com";
+    // const password = "123456";
+    // const credentials = await signInUser(email, password);
+    // console.log(credentials)
+    // const result = await signOutUser()
+    // console.log(result)
 });
 </script>
